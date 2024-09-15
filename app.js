8ae38d32074037503c88fc6ccc5faebe7c848d2f
rashadsafa rashadrashad <rashad257rs@mail.ru>
2024-09-15 20:00:54 +0000

    name

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        if (!$.trim($('input').val()) == '') {
+            let a = b => {
+                for (let i = 0; i < b; i++) { $('.box').append(`<h2>${b}</h2 > `); }
+            }
+            let v = f => Number(f) ? a(f) : $('.box').append(`<h2>${f}</h2>`);
+            v($('input').val());
+        }
+        $('input').val('');
+    })
+
 });
+
+
+
+
+
+
+
+
